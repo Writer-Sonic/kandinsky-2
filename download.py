7457a478667cdb0628c0f0d419e5047ceb2240cb
Daniel Treadwell <daniel@djt.id.au>
2023-01-19 15:26:44 +1000

    Latest

--- download.py
+++ download.py
@@ -5,8 +5,6 @@ from diffusers import StableDiffusionPipeline, LMSDiscreteScheduler
 import os
 
 def download_model():
-    # do a dry run of loading the huggingface model, which will download weights at build time
-    #Set auth token which is required to download stable diffusion model weights
     HF_AUTH_TOKEN = os.getenv("HF_AUTH_TOKEN")
 
     lms = LMSDiscreteScheduler(
