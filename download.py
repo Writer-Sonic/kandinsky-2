5d37fb92e8e844f9615195dbdc5a2b3c90abd7f0
Aryan Chaurasia <31214277+aryan1107@users.noreply.github.com>
2023-04-15 19:26:18 -0400

    Update download.py

--- download.py
+++ download.py
@@ -5,7 +5,7 @@ from kandinsky2 import get_kandinsky2
 
 def download_model():
     # Use 'cuda' if available, otherwise use 'cpu'
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    device = 0 if torch.cuda.is_available() else -1
     
     model = get_kandinsky2(device, task_type='text2img', cache_dir='/tmp/kandinsky2', model_version='2.1', use_flash_attention=False)
 
