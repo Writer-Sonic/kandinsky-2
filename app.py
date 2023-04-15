1ee0c013f1908d673dc00e8f825e9b0489b1a5e6
Aryan Chaurasia <31214277+aryan1107@users.noreply.github.com>
2023-04-15 01:12:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import os
 def init():
     global model
     HF_AUTH_TOKEN = os.getenv("HF_AUTH_TOKEN")
-    repo = 'CompVis/stable-diffusion-v1-4'
+    repo = 'SdValar/deliberate2'
     scheduler = DPMSolverMultistepScheduler.from_pretrained(repo, subfolder="scheduler")
     model = DiffusionPipeline.from_pretrained(repo, torch_dtype=torch.float16, revision="fp16", scheduler=scheduler, use_auth_token=HF_AUTH_TOKEN, safety_checker=None).to("cuda")    
 
