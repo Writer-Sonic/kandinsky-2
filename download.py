de30ecad062ed383cffee3190bdf41f4b646fa7c
Aryan Chaurasia <31214277+aryan1107@users.noreply.github.com>
2023-04-15 19:02:45 -0400

    Update download.py

--- download.py
+++ download.py
@@ -1,12 +1,11 @@
 import os
 import torch
-from diffusers import DiffusionPipeline, DPMSolverMultistepScheduler
+from kandinsky2 import get_kandinsky2
+
 
 def download_model():
-    HF_AUTH_TOKEN = os.getenv("HF_AUTH_TOKEN")
-    repo = 'SdValar/deliberate2'
-    scheduler = DPMSolverMultistepScheduler.from_pretrained(repo, subfolder="scheduler")
-    model = DiffusionPipeline.from_pretrained(repo, scheduler=scheduler, use_auth_token=HF_AUTH_TOKEN, safety_checker=None) 
+    model = get_kandinsky2('cuda', task_type='text2img', cache_dir='/tmp/kandinsky2', model_version='2.1', use_flash_attention=False)
+
     
 if __name__ == "__main__":
     download_model()
