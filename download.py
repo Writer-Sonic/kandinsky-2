5cc3cfe6429d742bbb29c1c2a205902bfe2d8ad0
Aryan Chaurasia <31214277+aryan1107@users.noreply.github.com>
2023-04-15 01:12:37 -0400

    Update download.py

--- download.py
+++ download.py
@@ -4,7 +4,7 @@ from diffusers import DiffusionPipeline, DPMSolverMultistepScheduler
 
 def download_model():
     HF_AUTH_TOKEN = os.getenv("HF_AUTH_TOKEN")
-    repo = 'CompVis/stable-diffusion-v1-4'
+    repo = 'SdValar/deliberate2'
     scheduler = DPMSolverMultistepScheduler.from_pretrained(repo, subfolder="scheduler")
     model = DiffusionPipeline.from_pretrained(repo, torch_dtype=torch.float16, revision="fp16", scheduler=scheduler, use_auth_token=HF_AUTH_TOKEN, safety_checker=None) 
     
